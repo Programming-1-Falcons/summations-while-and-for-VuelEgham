581fb73bad4875b929618de86ccad2c8ea2f5794
Parker Gustavson <gustapar000@alpinesd.org>
2024-10-22 11:13:09 -0600

    Update while_summation.py

--- while_summation.py
+++ while_summation.py
@@ -1,1 +1,10 @@
-#while summation code following directions from Readme
+
+user_num=input(int(What is your number?))
+
+loopedamount=0
+
+while loopedamount<=user_num
+
+    print(loopedamount)
+
+    loopedamount = loopedamount + 1
